2083a15c78563d41c4cacec5b5e5a81a9fcb7725
Daniel <danieldicksondillimono@gmail.com>
2024-03-01 22:53:56 +0100

    #4 added maps controller class i.e the backend for map functionality. Hard coded search terms can be viewed on the map. Still need to connect the searchBar to the search function and make the map pan to the searched item coordinate

--- appmanager.cpp
+++
@@ -1,8 +0,0 @@
-#include "appmanager.h"
-#include "qdebug.h"
-
-AppManager::AppManager(QObject *parent)
-    : QObject{parent}
-{
-    qDebug() << "class created";
-}
--- src/controllers/mapscontroller.cpp
+++
@@ -1,5 +0,0 @@
-#include "mapscontroller.h"
-
-MapsController::MapsController(QObject *parent)
-    : QObject{parent}
-{}
